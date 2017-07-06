349140b17e5d469487c1e596b5dda2a93ef7d434
Muhammad Osama Arshad <osama.arshad@arbisoft.com>
2017-07-06 14:23:13 +0500

    added custom load data command

--- weather/management/commands/load_dummy_data.py
+++ weather/management/commands/load_dummy_data.py
@@ -6,11 +6,28 @@ from django.utils import timezone
 from django.core.management.base import BaseCommand
 
 
-
 class Command(BaseCommand):
-    def handle(self, **options):
+    def handle(self, **options):        
+        WeatherModel.objects.all().delete()
         with open('madrid.csv', 'r') as csvfile:
             lines = csv.DictReader(csvfile)
+            db_lines = []
             for line in lines:
-                print(line)
-    
+                line.pop('max_wind_speed')
+                line.pop('mean_wind_speed')
+                line.pop('max_gust_speed')
+                line.pop('precipitation')
+                line.pop('cloud_cover')
+                line.pop('events')
+                line.pop('wind_direction')
+                for key in line:
+                    if key != 'date':
+                        try:
+                            line[key] = float(line[key])
+                        except ValueError:
+                            line[key] = 0
+                db_lines.append(line)
+                # print(line)
+            # print([WeatherModel(**line) for line in db_lines])
+            WeatherModel.objects.bulk_create([WeatherModel(**line) for line in db_lines])
+                # print(instance.wind_direction)
