be0d903045b25c22e47cb313fecd0f7aa84ab6b5
Muhammad Osama Arshad <osama.arshad@arbisoft.com>
2017-07-06 16:42:05 +0500

    added summary endpoint

--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,6 @@
 import json
+from datetime import date
+from collections import OrderedDict
 from django.shortcuts import render
 from rest_framework import viewsets, mixins
 from rest_framework.views import APIView
@@ -21,11 +23,49 @@ class WeatherSummaryView(APIView):
     '''
     gives summary of the weather data
     '''
+
     def get(self, request, format=None):
-        weather = WeatherModel.objects.all()
-        serializer = WeatherSerializer(weather)
-        summary_dict = {
-            'mean_temp': 100,
-            'max_temp': 1000,
-        }
+        params = request.query_params
+        startdate = date.fromordinal(int(
+            params['startdate'])) if 'startdate' in params else date(
+            date.today().year, 1, 1)
+        enddate = date.fromordinal(int(
+            params['enddate'])) if 'enddate' in params else date.today()
+        items = int(params['items']) if 'items' in params else 10
+        summary_dict = OrderedDict([
+            ('max_temp', []),
+            ('mean_temp', []),
+            ('min_temp', []),
+            ('max_dew', []),
+            ('mean_dew', []),
+            ('min_dew', []),
+            ('max_humidity', []),
+            ('mean_humidity', []),
+            ('min_humidity', []),
+            ('max_sea_pressure', []),
+            ('mean_sea_pressure', []),
+            ('min_sea_pressure', []),
+            ('max_visibility', []),
+            ('mean_visibility', []),
+            ('min_visibility', []),
+        ])
+        for key in ['temp', 'dew', 'humidity', 'sea_pressure', 'visibility']:
+            max_key = 'max_' + key
+            mean_key = 'mean_' + key
+            min_key = 'min_' + key
+            max_ = [getattr(instance, max_key) for instance in WeatherModel.objects.filter(
+                    date__gte=startdate, date__lte=enddate).order_by(
+                        '-' + max_key)[:items]]
+            _ = [getattr(instance, mean_key)
+                 for instance in WeatherModel.objects.all()[:items]]
+            mean_ = sum(_) / len(_)
+
+            min_ = [getattr(instance, min_key) for instance in WeatherModel.objects.filter(
+                    date__gte=startdate, date__lte=enddate).order_by(min_key)[:items]]
+            summary_dict['max_' + key] = max_
+            summary_dict['mean_' + key] = mean_
+            summary_dict['min_' + key] = min_
+        # min_temps = WeatherModel.objects.order_by('min_temp')[:10
+
+        # serializer = WeatherSerializer(max_temp, many=True)
         return Response(summary_dict)
